b872b08e587e1817803f610bf72f5a5a0b13f0c5
Hwijoon Lim <wjuni@kaist.ac.kr>
2022-04-19 13:09:42 +0900

    Assignment 4 pre-release

--- assignments/assembly/resource/mydc.s
+++ assignments/assembly/resource/mydc.s
@@ -1,106 +1,125 @@
-### --------------------------------------------------------------------
-### mydc.s
-###
-### Desk Calculator (dc)
-### --------------------------------------------------------------------
+# --------------------------------------------------------------------
+# dc.s
+#
+# Desk Calculator (dc) (x86-64)
+# 
+# Student ID: 2022xxxx
+# --------------------------------------------------------------------
 
-	.equ   ARRAYSIZE, 20
-	.equ   EOF, -1
+    .equ   BUFFERSIZE, 32
+    .equ   EOF, -1
+
+# --------------------------------------------------------------------
 
 .section ".rodata"
 
 scanfFormat:
-	.asciz "%s"
-### --------------------------------------------------------------------
+     .asciz "%s"
 
-        .section ".data"
+# --------------------------------------------------------------------
 
-### --------------------------------------------------------------------
+    .section ".data"
 
-        .section ".bss"
+# --------------------------------------------------------------------
+
+    .section ".bss"
+
 buffer:
-        .skip  ARRAYSIZE
+    .skip  BUFFERSIZE
 
-### --------------------------------------------------------------------
+stackBase:
+    .skip  4
+# --------------------------------------------------------------------
+    .section ".text"
 
-	.section ".text"
+    # -------------------------------------------------------------
+    # int powerfunc(int base, int exponent)
+    # Runs the power function.  Returns result.
+    # -------------------------------------------------------------
+    
+    .globl	powerfunc
+    .type   powerfunc, @function
 
-	## -------------------------------------------------------------
-	## int main(void)
-	## Runs desk calculator program.  Returns 0.
-	## -------------------------------------------------------------
+    # base is stored in %rdi
+    # exponent is stored in %rsi
 
-	.globl  main
-	.type   main,@function
+powerfunc:
+
+    ret            # return result
+
+# -------------------------------------------------------------
+# int main(void)
+# Runs desk calculator program.  Returns 0.
+# -------------------------------------------------------------
+
+    .text
+    .globl  main
+    .type   main, @function
 
 main:
 
-	pushl   %ebp
-	movl    %esp, %ebp
+    pushq   %rbp
+    movq    %rsp, %rbp
 
-input:
+    # char buffer[BUFFERSIZE]
+    subq    $BUFFERSIZE, %rsp
 
-	## dc number stack initialized. %esp = %ebp
+    # Note %rsp must be 16-B aligned before call
 
-	## scanf("%s", buffer)
-	pushl	$buffer
-	pushl	$scanfFormat
-	call    scanf
-	addl    $8, %esp
+.input:
+    
+    # scanf("%s", buffer)
+    # set al to 0, as there is no FP reg. args
+    # see https://stackoverflow.com/questions/6212665/why-is-eax-zeroed-before-a-call-to-printf
+    leaq    scanfFormat, %rdi
+    leaq    -BUFFERSIZE(%rbp), %rsi
+    movb    $0, %al
+    call    scanf
 
-	## check if user input EOF
-	cmp	$EOF, %eax
-	je	quit
+    # check if user input == EOF
+    cmp	    $EOF, %eax
+    je	    .quit
 
-	## PSEUDO-CODE
-	## /*
-	##  * In this pseudo-code we are assuming that no local variables are created
-	##  * in the _main_ process stack. In case you want to allocate space for local
-	##  * variables, please remember to update logic for 'empty dc stack' condition
-	##  * (lines 6, 15 and 20) accordingly.
-	##  */
-	##
-	##1 while (1) {
-	##2	if (scanf("%s", buffer) == EOF)
-	##3		return 0;
-	##4 	if (!isdigit(buffer[0])) {
-	##5		if (buffer[0] == 'p') {
-	##6			if (stack.peek() == NULL) { /* is %esp == %ebp? */
-	##7				fprintf(stderr, "dc: stack empty\n");
-	##8			} else {
-	##9				printf("%d\n", (int)stack.top()); /* value is already pushed in the stack */
-	##10			}
-	##11		} else if (buffer[0] == 'q') {
-	##12			goto quit;
-	##13		} else if (buffer[0] == '+') {
-	##14			int a, b;
-	##15			if (stack.peek() == NULL) {
-	##16				fprintf(stderr, "dc: stack empty\n");
-	##17				continue;
-	##18			}
-	##19			a = (int)stack.pop();
-	##20			if (stack.peek() == NULL) {
-	##21				fprintf(stderr, "dc: stack empty\n");
-	##22				stack.push(a); /* pushl some register value */
-	##23				continue;
-	##24			}
-	##25			b = (int)stack.pop(); /* popl to some register */
-	##26			res = a + b;
-	##27 			stack.push(res);
-	##28		} else if (buffer[0] == '-') {
-	##29			/* ... */
-	##30		} else if (buffer[0] == '^') {
-	##31			/* ... powerfunc() ... */
-	##32		} else if { /* ... and so on ... */
-	##33 	} else { /* the first no. is a digit */
-	##34		int no = atoi(buffer);
-	##35		stack.push(no);	/* pushl some register value */
-	##36	}
-	##37 }
+    # while (1) {
+    # 	if (!isdigit(buffer[0])) {
+    #		if (buffer[0] == 'p') {
+    #			if (stack.peek() == NULL) { /* is %esp == %ebp */
+    #				printf("Stack is empty!\n");
+    #			} else {
+    #				printf("%d\n", (int)stack.pop());
+    #			}
+    #		} else if (buffer[0] == 'q') {
+    #			return 0;
+    #		} else if (buffer[0] == '+') {
+    #			int a, b;
+    #			if (stack.peek() == NULL) {
+    #				printf("Stack is empty!\n");
+    #				continue;
+    #			}
+    #			a = (int)stack.pop();
+    #			if (stack.peek() == NULL) {
+    #				printf("Stack is empty!\n");
+    #				stack.push(a);
+    #				continue;
+    #			}
+    #			b = (int)stack.pop();
+    #			res = a + b;
+    # 			stack.push(res);
+    #		} else if (buffer[0] == '-') {
+    #			/* ... */
+    #		} else if (buffer[0] == '^') {
+    #			/* ... func() ... */
+    #		} else if { /* ... and so on ... */
+    # 	} else { /* the first no. is a digit */
+    #		int no = atoi(buffer);
+    #		stack.push(no);
+    # 	}
+    # }
 
-quit:
-	## return 0
-	movl    $0, %eax
-	movl    %ebp, %esp
-	popl    %ebp
-	ret
+.quit:	
+    # return 0
+    movq    $0, %rax
+    addq    $BUFFERSIZE, %rsp
+    movq    %rbp, %rsp
+    popq    %rbp
+    ret
