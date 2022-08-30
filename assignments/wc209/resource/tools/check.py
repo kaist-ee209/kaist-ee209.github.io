8f22b44f7c1b21b94a6ad35dc991940eb8a3cf84
geonha <geonha@kaist.ac.kr>
2022-08-30 20:55:03 +0900

    Fix for old python (assignment 1)

--- assignments/wc209/resource/tools/check.py
+++ assignments/wc209/resource/tools/check.py
@@ -10,7 +10,7 @@ import subprocess
 FILES = ['wc209.c', 'dfa.pdf', 'readme', 'EthicsOath.pdf']
 
 def fatal(msg):
-    print(f'[-] ERROR: {msg}')
+    print('[-] ERROR: {}'.format(msg))
     sys.exit(1)
 
 def parse_args():
@@ -30,13 +30,13 @@ if __name__ == '__main__':
 
     # General check
     if not os.path.exists(tar):
-        fatal(f'{tar} does not exist')
+        fatal('{} does not exist'.format(tar))
 
     sid = get_student_id()
 
-    expected_filename = f'{sid}_assign1.tar.gz'
+    expected_filename = '{}_assign1.tar.gz'.format(sid)
     if os.path.basename(tar) != expected_filename:
-        fatal(f'A file name is wrong (expected: {expected_filename})')
+        fatal('A file name is wrong (expected: {})'.format(expected_filename))
         sys.exit(1)
 
     if not tarfile.is_tarfile(tar):
@@ -50,13 +50,13 @@ if __name__ == '__main__':
     for f in tar:
         name = f.name
         if not name in files:
-            fatal(f'{name} is incorrectly included')
+            fatal('{} is incorrectly included'.format(name))
 
         files.remove(name)
 
     if files:
         for name in files:
-            fatal(f'{name} should be included')
+            fatal('{} should be included'.format(name))
 
     # Check whether wc209.c can be compiled with gcc209
     if not shutil.which('gcc209'):
